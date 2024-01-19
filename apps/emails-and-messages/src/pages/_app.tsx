ea00cfda6ae281d38d295f0a08b8e603d0bd5eb5
ManotLuijiu <manot_luijiu@hotmail.com>
2024-01-19 11:27:03 +0700

    Fixed saleorApiUrl in email

--- apps/emails-and-messages/src/pages/_app.tsx
+++ apps/emails-and-messages/src/pages/_app.tsx
@@ -15,7 +15,12 @@ import { trpcClient } from "../modules/trpc/trpc-client";
  * Ensure instance is a singleton.
  * TODO: This is React 18 issue, consider hiding this workaround inside app-sdk
  */
-export const appBridgeInstance = typeof window !== "undefined" ? new AppBridge() : undefined;
+export const appBridgeInstance =
+  typeof window !== "undefined"
+    ? new AppBridge({
+        saleorApiUrl: "https://saleor-api.iesthete.com/graphql/",
+      })
+    : undefined;
 
 function NextApp({ Component, pageProps }: AppProps) {
   return (
